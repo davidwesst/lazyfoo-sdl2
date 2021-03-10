f06105c6a1095dc25af2c04b694901e616b57533
David Wesst <david@cocobokostudios.com>
2021-03-10 00:03:34 -0600

    init commit with hello world SDL

---
+++ 01_hello_SDL2.cpp
@@ -0,0 +1,37 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 640;
+
+int main( int argc, char* args[] )
+{
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    // initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "Oh no! SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        // create window
+        window = SDL_CreateWindow( "SDL2 Tutorial by DW", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if( window == NULL )
+        {
+            printf("Oh no! Window was not created! SDL Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            screenSurface = SDL_GetWindowSurface( window );
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 100, 255, 100));
+            SDL_UpdateWindowSurface( window );
+
+            SDL_Delay(1000);
+        }
+    }
+
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# lazyfoo-sdl2
+My repository for following along with the Lazy Foo Productions game development tutorial using SDL2. [LINK](http://lazyfoo.net/tutorials/SDL/index.php#Hello%20SDL).
+
+
+
+
+Copyright Notice:
+-----------------
+The files within this zip file are copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.
+
+This project is linked against:
+----------------------------------------
+Windows:
+SDL2
+SDL2main
+
+*nix:
+SDL2
